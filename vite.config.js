80396f61a32881e9977009e07fe9cce6b34e8d55
Agonizingfool <edwardpritchard@me.com>
2026-03-19 12:41:51 +1100

    chore: nuke vanilla setup, migrate to react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-first-react-app/', // Replace this with your Repository Name
+  base: '/my-first-react-app/', // Smart addition for GitHub Pages deployment
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
